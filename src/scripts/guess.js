0b260d6333499554059f967bb25889300385d222
Afraz-2005 <mahbirafraz2007@gmail.com>
2021-10-06 11:26:24 +0600

    updated

--- src/scripts/guess.js
+++ src/scripts/guess.js
@@ -1,4 +1,11 @@
-let myComputerElements = ['U ARE CURSED', 'Someone shall fulfill your 3 wishes. Contact me, I might be the one too', 'Your future is dark', "I'm gonna put some dirt in your eye", "BE AWARE!"];
+let myComputerElements = [
+    'U ARE CURSED', 
+    'Someone shall fulfill your 3 wishes. Contact me, I might be the one too', 
+    'Your future is dark', 
+    "I'm gonna put some dirt in your eye", 
+    "You shall shine bright like a diamond",
+    "You are the choosen future president of your country"
+];
 
 setTimeout(function () {
   let compRand = myComputerElements[Math.floor(Math.random() * myComputerElements.length)];
